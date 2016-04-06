257fd57778a4b0bfe2466ab3c6b05d1841fba181
John Knoll <jpknoll@ucdavis.edu>
2016-04-06 16:08:09 -0700

    strip out account and auth stuff for cas

--- AD-419_DataHelperWebApp/App_Start/Startup.Auth.cs
+++ AD-419_DataHelperWebApp/App_Start/Startup.Auth.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.Google;
 using Owin;
 using AD_419_DataHelperWebApp.Models;
 
@@ -17,7 +16,6 @@ namespace AD_419_DataHelperWebApp
             // Configure the db context, user manager and signin manager to use a single instance per request
             app.CreatePerOwinContext(ApplicationDbContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
-            app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
 
             // Enable the application to use a cookie to store information for the signed in user
             // and to use a cookie to temporarily store information about a user logging in with a third party login provider
@@ -36,33 +34,6 @@ namespace AD_419_DataHelperWebApp
                 }
             });            
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
-
-            // Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
-            app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
-
-            // Enables the application to remember the second login verification factor such as phone or email.
-            // Once you check this option, your second step of verification during the login process will be remembered on the device where you logged in from.
-            // This is similar to the RememberMe option when you log in.
-            app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
-
-            // Uncomment the following lines to enable logging in with third party login providers
-            //app.UseMicrosoftAccountAuthentication(
-            //    clientId: "",
-            //    clientSecret: "");
-
-            //app.UseTwitterAuthentication(
-            //   consumerKey: "",
-            //   consumerSecret: "");
-
-            //app.UseFacebookAuthentication(
-            //   appId: "",
-            //   appSecret: "");
-
-            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            //{
-            //    ClientId = "",
-            //    ClientSecret = ""
-            //});
         }
     }
 }
---
+++ AD-419_DataHelperWebApp/Helpers/CASHelper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Web;
+using System.Web.Security;
+
+namespace AD_419_DataHelperWebApp.Helpers
+{
+    public static class CasHelper
+    {
+        private const string StrCasUrl = "https://cas.ucdavis.edu/cas/";
+        private const string StrTicket = "ticket";
+        private const string StrReturnUrl = "ReturnURL";
+
+        public static string GetReturnUrl()
+        {
+            return HttpContext.Current.Request.QueryString["ReturnURL"];
+        }
+
+        public static void LoginAndRedirect()
+        {
+            var url = Login();
+            if (url == null)
+                return;
+            HttpContext.Current.Response.Redirect(url);
+        }
+
+        public static string Login()
+        {
+            var current = HttpContext.Current;
+            var httpCookie = current.Request.Cookies[FormsAuthentication.FormsCookieName];
+            var authenticationTicket = (FormsAuthenticationTicket)null;
+            if (httpCookie != null)
+            {
+                try
+                {
+                    authenticationTicket = FormsAuthentication.Decrypt(httpCookie.Value);
+                }
+                catch
+                {
+                    authenticationTicket = null;
+                }
+            }
+            if (authenticationTicket != null && !authenticationTicket.Expired) return null;
+
+            var str1 = "";
+            foreach (var strA in current.Request.QueryString.AllKeys)
+            {
+                if (string.Compare(strA, "ticket", StringComparison.OrdinalIgnoreCase) != 0)
+                    str1 = str1 + "&" + strA + "=" + current.Request.QueryString[strA];
+            }
+            if (!string.IsNullOrEmpty(str1))
+                str1 = "?" + str1.Substring(1);
+            var str2 = current.Request.QueryString["ticket"];
+            var str3 = current.Server.UrlEncode(current.Request.Url.GetLeftPart(UriPartial.Path) + str1);
+            if (!string.IsNullOrEmpty(str2))
+            {
+                var stream = (new WebClient()).OpenRead("https://cas.ucdavis.edu/cas/validate?ticket=" + str2 + "&service=" + str3);
+                var streamReader = new StreamReader(stream);
+                if (streamReader.ReadLine() == "yes")
+                {
+                    FormsAuthentication.SetAuthCookie(streamReader.ReadLine(), false);
+                    var returnUrl = CasHelper.GetReturnUrl();
+                    if (string.IsNullOrEmpty(returnUrl))
+                        return FormsAuthentication.DefaultUrl;
+                    return returnUrl;
+                }
+            }
+            current.Response.Redirect("https://cas.ucdavis.edu/cas/login?service=" + str3);
+            return null;
+        }
+    }
+}
