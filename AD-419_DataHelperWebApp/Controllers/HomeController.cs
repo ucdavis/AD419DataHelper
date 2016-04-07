691448acf6fc5e92952a711c9d00d2aa4ff634a3
John Knoll <jpknoll@ucdavis.edu>
2016-04-07 14:57:10 -0700

    unused views

--- AD-419_DataHelperWebApp/Controllers/HomeController.cs
+++ AD-419_DataHelperWebApp/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace AD_419_DataHelperWebApp.Controllers
@@ -10,20 +7,6 @@ namespace AD_419_DataHelperWebApp.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
