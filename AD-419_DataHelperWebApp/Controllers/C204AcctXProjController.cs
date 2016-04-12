be3bbbc395f5957b0bb7f391e44fcbea90931fc0
Ken Taylor <kentaylor@ucdavis.edu>
2016-04-12 09:21:42 -0700

    Added 204AcctXProj controller and files.

---
+++ AD-419_DataHelperWebApp/Controllers/C204AcctXProjController.cs
@@ -0,0 +1,123 @@
+﻿using AD_419_DataHelperWebApp.Models;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace AD_419_DataHelperWebApp.Controllers
+{
+    public class C204AcctXProjController : Controller
+    {
+        private AD419DataContext db = new AD419DataContext();
+
+        // GET: C204AcctXProj
+        public ActionResult Index()
+        {
+            return View(db.C204AcctXProj.ToList());
+        }
+
+        // GET: C204AcctXProj/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            C204AcctXProj c204AcctXProj = db.C204AcctXProj.Find(id);
+            if (c204AcctXProj == null)
+            {
+                return HttpNotFound();
+            }
+            return View(c204AcctXProj);
+        }
+
+        // GET: C204AcctXProj/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: C204AcctXProj/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "pk,AccountID,Expenses,Accession,Chart,Is219,CSREES_ContractNo,AwardNum,IsCurrentProject,Org,OrgR,IsExcludedExpense")] C204AcctXProj c204AcctXProj)
+        {
+            if (ModelState.IsValid)
+            {
+                db.C204AcctXProj.Add(c204AcctXProj);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(c204AcctXProj);
+        }
+
+        // GET: C204AcctXProj/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            C204AcctXProj c204AcctXProj = db.C204AcctXProj.Find(id);
+            if (c204AcctXProj == null)
+            {
+                return HttpNotFound();
+            }
+            return View(c204AcctXProj);
+        }
+
+        // POST: C204AcctXProj/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "pk,AccountID,Expenses,Accession,Chart,Is219,CSREES_ContractNo,AwardNum,IsCurrentProject,Org,OrgR,IsExcludedExpense")] C204AcctXProj c204AcctXProj)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(c204AcctXProj).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(c204AcctXProj);
+        }
+
+        // GET: C204AcctXProj/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            C204AcctXProj c204AcctXProj = db.C204AcctXProj.Find(id);
+            if (c204AcctXProj == null)
+            {
+                return HttpNotFound();
+            }
+            return View(c204AcctXProj);
+        }
+
+        // POST: C204AcctXProj/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            C204AcctXProj c204AcctXProj = db.C204AcctXProj.Find(id);
+            db.C204AcctXProj.Remove(c204AcctXProj);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- AD-419_DataHelperWebApp/Models/C204AcctXProj.cs
+++ AD-419_DataHelperWebApp/Models/C204AcctXProj.cs
@@ -1,45 +1,59 @@
 namespace AD_419_DataHelperWebApp.Models
 {
-    using System;
-    using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
 
-    [Table("204AcctXProj")]
+    [Table("[204AcctXProj]")]
     public partial class C204AcctXProj
     {
         [Required]
         [StringLength(7)]
+        [MinLength(7)]
+        [Display(Name = "Account Number")]
         public string AccountID { get; set; }
 
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:C}")]
         public double Expenses { get; set; }
 
-        public double? DividedAmount { get; set; }
-
         [StringLength(7)]
+        [MinLength(7)]
+        [Required]
         public string Accession { get; set; }
 
         [StringLength(1)]
+        [MinLength(1)]
+        [Required]
         public string Chart { get; set; }
 
         [Key]
         public int pk { get; set; }
 
+        [Display(Name = "Is 219?")]
         public bool? Is219 { get; set; }
 
         [StringLength(20)]
+        [Display(Name = "Project Contract Number")]
         public string CSREES_ContractNo { get; set; }
 
         [StringLength(20)]
+        [Display(Name = "Account Award Number")]
         public string AwardNum { get; set; }
 
+        [Display(Name = "Is Current AD-419 Project?")]
         public bool? IsCurrentProject { get; set; }
 
         [StringLength(4)]
+        [MinLength(4)]
+        [Required]
         public string Org { get; set; }
 
         [StringLength(4)]
+        [MinLength(7)]
+        [Required]
         public string OrgR { get; set; }
+
+        [Display(Name = "Is Excluded Expense?")]
+        public bool? IsExcludedExpense { get; set; }
     }
 }
