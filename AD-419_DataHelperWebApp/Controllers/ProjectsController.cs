4f66076e1bd757f48be92ebd07195210158262b7
John Knoll <jpknoll@ucdavis.edu>
2016-04-25 16:13:44 -0700

    unused usings

--- AD-419_DataHelperWebApp/Controllers/ProjectsController.cs
+++ AD-419_DataHelperWebApp/Controllers/ProjectsController.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using AD_419_DataHelperWebApp.Models;
 
