6e0bff157fb22099d481a677e80a166a349faa0f
Ken Taylor <kentaylor@ucdavis.edu>
2016-05-18 15:40:53 -0700

    Added support for ReportViewerMVC.

---
+++ AD419.DataHelper.Web/Controllers/FTEReportController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using Microsoft.Reporting.WebForms;
+
+namespace AD419.DataHelper.Web.Controllers
+{
+    public class FteReportController : Controller
+    {
+        // GET: FTEReport
+        public ActionResult Index()
+        {
+            var reportViewer = new ReportViewer
+            {
+                SizeToReportContent = true,
+                Width = Unit.Percentage(100),
+                Height = Unit.Percentage(100),
+                ProcessingMode = ProcessingMode.Remote
+            };
+
+            reportViewer.ServerReport.ReportPath = "/AD419Reports/FTE Greater than 1";
+            reportViewer.ServerReport.ReportServerUrl = new Uri("http://testreports.caes.ucdavis.edu/ReportServer/");
+
+            ViewBag.ReportViewer = reportViewer;
+            return View();
+        }
+    }
+}
