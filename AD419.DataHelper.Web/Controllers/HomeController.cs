e233486189e9004a31128a8701478e07eee725a5
John Knoll <jpknoll@ucdavis.edu>
2016-06-06 15:03:19 -0700

    use super controller

--- AD419.DataHelper.Web/Controllers/HomeController.cs
+++ AD419.DataHelper.Web/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Web.Mvc;
 
 namespace AD419.DataHelper.Web.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : SuperController
     {
         public HomeController()
         {
