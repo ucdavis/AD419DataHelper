f47b67aea58b573e5d3442817a61585e0b764d22
Ken Taylor <kentaylor@ucdavis.edu>
2016-06-06 12:22:03 -0700

    Added FiscalYear to home page.

--- AD419.DataHelper.Web/Controllers/HomeController.cs
+++ AD419.DataHelper.Web/Controllers/HomeController.cs
@@ -1,9 +1,20 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 
 namespace AD419.DataHelper.Web.Controllers
 {
     public class HomeController : Controller
     {
+        public HomeController()
+        {
+            var session = System.Web.HttpContext.Current.Session;
+            var fiscalYear = session["FiscalYear"];
+            if (fiscalYear == null)
+            {
+                fiscalYear = DateTime.Now.Year - 1;
+                session["FiscalYear"] = fiscalYear;
+            }
+        }
         public ActionResult Index()
         {
             return View();
