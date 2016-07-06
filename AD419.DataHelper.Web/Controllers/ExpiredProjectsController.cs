d0d04f7d6e42b0c58e4ad27be543d2db6c02e75e
John Knoll <jpknoll@ucdavis.edu>
2016-07-06 16:36:40 -0700

    rename controller

---
+++ AD419.DataHelper.Web/Controllers/ExpiredProjectsController.cs
@@ -0,0 +1,107 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using AD419.DataHelper.Web.Models;
+
+namespace AD419.DataHelper.Web.Controllers
+{
+    public class ExpiredProjectsController : SuperController
+    {
+        // GET: ExpiredProjectCrossReference
+        public ActionResult Index()
+        {
+            var projects = DbContext.ExpiredProjectCrossReference.ToList();
+            return View(projects);
+        }
+
+        // GET: ExpiredProjectCrossReference/Details/5
+        public ActionResult Details(int id)
+        {
+            var project = DbContext.ExpiredProjectCrossReference.Find(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(project);
+        }
+
+        // GET: ExpiredProjectCrossReference/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ExpiredProjectCrossReference/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RemapID,FromAccession,IsActive,ToAccession")] ExpiredProjectCrossReference expiredProjectCrossReference)
+        {
+            if (!ModelState.IsValid) return View(expiredProjectCrossReference);
+
+            DbContext.ExpiredProjectCrossReference.Add(expiredProjectCrossReference);
+            DbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: ExpiredProjectCrossReference/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var project = DbContext.ExpiredProjectCrossReference.Find(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(project);
+        }
+
+        // POST: ExpiredProjectCrossReference/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RemapID,FromAccession,IsActive,ToAccession")] ExpiredProjectCrossReference expiredProjectCrossReference)
+        {
+            if (!ModelState.IsValid) return View(expiredProjectCrossReference);
+
+            DbContext.Entry(expiredProjectCrossReference).State = EntityState.Modified;
+            DbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: ExpiredProjectCrossReference/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var project = DbContext.ExpiredProjectCrossReference.Find(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(project);
+        }
+
+        // POST: ExpiredProjectCrossReference/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var project = DbContext.ExpiredProjectCrossReference.Find(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+
+            DbContext.ExpiredProjectCrossReference.Remove(project);
+            DbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
