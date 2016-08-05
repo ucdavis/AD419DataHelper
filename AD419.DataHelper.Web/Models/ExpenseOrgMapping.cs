935613cd747847792ab5c44f87da6ebb3f9b4feb
John Knoll <jpknoll@gmail.com>
2016-08-05 15:40:56 -0700

    new table

---
+++ AD419.DataHelper.Web/Models/ExpenseOrgMapping.cs
@@ -0,0 +1,19 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AD419.DataHelper.Web.Models
+{
+    [Table("ExpenseOrgR_X_AD419OrgR")]
+    public class ExpenseOrgMapping
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Chart { get; set; }
+
+        public string ExpenseOrgR { get; set; }
+
+        public string AD419OrgR { get; set; }
+    }
+}
