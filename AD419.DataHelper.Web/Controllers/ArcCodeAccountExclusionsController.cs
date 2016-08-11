e44bcdd919c3037922d34fe2a3011201379233f6
John Knoll <jpknoll@gmail.com>
2016-08-11 11:29:02 -0700

    import functionality
    Fixes #45

--- AD419.DataHelper.Web/Controllers/ArcCodeAccountExclusionsController.cs
+++ AD419.DataHelper.Web/Controllers/ArcCodeAccountExclusionsController.cs
@@ -25,6 +25,49 @@ namespace AD419.DataHelper.Web.Controllers
             return View(exclusions);
         }
 
+        [HttpPost]
+        public ActionResult ImportOld()
+        {
+            // get this year's data
+            var year = FiscalYearService.FiscalYear;
+            var current = DbContext.ArcCodeAccountExclusions
+                .Where(x => x.Year == year)
+                .ToList();
+
+            // get last year's data
+            var last = year - 1;
+            var previous = DbContext.ArcCodeAccountExclusions
+                .Where(x => x.Year == last)
+                .ToList();
+
+            // match on the other 3 keys
+            var matches = from p in previous 
+                          join c in current on new { p.Chart, p.Account, p.AnnualReportCode } equals new { c.Chart, c.Account, c.AnnualReportCode } into g
+                          from c2 in g.DefaultIfEmpty()
+                          select new { Previous = p, Match = c2 };
+
+            // get ones missing a match
+            var missing = matches.Where(m => m.Match == null).Select(m => m.Previous);
+
+            // map to new year
+            var imports = missing.Select(m => new ArcCodeAccountExclusion
+            {
+                Year             = year,
+                Chart            = m.Chart,
+                Account          = m.Account,
+                AnnualReportCode = m.AnnualReportCode,
+                Is204            = m.Is204,
+                ProjectNumber    = m.ProjectNumber,
+                AwardNumber      = m.AwardNumber
+            });
+
+            // insert
+            DbContext.ArcCodeAccountExclusions.AddRange(imports);
+            DbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
         // GET: ArcCodeAccountExclusions/Details/5
         public ActionResult Details(int? year, string chart, string account, string annualReportCode)
         {
