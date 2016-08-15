d9c975cfd58ad7713701e671d43e8ee872e5fbc3
John Knoll <jpknoll@gmail.com>
2016-08-15 16:28:12 -0700

    names

--- AD419.DataHelper.Web/Models/PrincipalInvestigatorMatch.cs
+++ AD419.DataHelper.Web/Models/PrincipalInvestigatorMatch.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AD419.DataHelper.Web.Models
 {
+    [Table("PI_Match")]
     public class PrincipalInvestigatorMatch
     {
         [Key]
@@ -17,9 +18,11 @@ namespace AD419.DataHelper.Web.Models
         public string Name { get; set; }
 
         [Column("EID")]
+        [DisplayName("Employee ID")]
         public string EmployeeId { get; set; }
 
         [Column("PI_Match")]
+        [DisplayName("Match Name")]
         public string MatchName { get; set; }
 
         [DisplayName("Is Prorated?")]
