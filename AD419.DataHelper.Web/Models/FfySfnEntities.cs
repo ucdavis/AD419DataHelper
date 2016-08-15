45b56f04b23f8963a221433ea25e8eead58ad7b4
John Knoll <jpknoll@gmail.com>
2016-08-15 09:15:23 -0700

    new model

---
+++ AD419.DataHelper.Web/Models/FfySfnEntities.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AD419.DataHelper.Web.Models
+{
+    [Table("FFY_SFN_Entities")]
+    public class FfySfnEntities
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Chart { get; set; }
+
+        public string Account { get; set; }
+
+        public string Sfn { get; set; }
+
+        [Column("Accounts_AwardNum")]
+        public string AccountsAwardNumber { get; set; }
+
+        [Column("OpFund_AwardNum")]
+        public string OpFundAwardNumber { get; set; }
+
+        public string AccessionNumber { get; set; }
+
+        public string ProjectNumber { get; set; }
+
+        public bool IsExpired { get; set; }
+
+        public DateTime ProjectEndDate { get; set; }
+
+        public decimal Expenses { get; set; }
+
+        [Column("FTE")]
+        public decimal FullTimeEmployees { get; set; }
+    }
+}
