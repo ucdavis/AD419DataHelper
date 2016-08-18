e7032805f90d44e747c6955c11731fc3e3c1c331
John Knoll <jpknoll@gmail.com>
2016-08-18 11:02:51 -0700

    new model

---
+++ AD419.DataHelper.Web/Models/ProcessStatus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AD419.DataHelper.Web.Models
+{
+    [Table("ProcessStatus")]
+    public class ProcessStatus
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [DisplayName("Is Completed?")]
+        public bool IsCompleted { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
