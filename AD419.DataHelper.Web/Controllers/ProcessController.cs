a9530feee9889145fa2e13870596b34307c28b59
Ken Taylor <kentaylor@ucdavis.edu>
2016-08-19 15:04:49 -0700

    Initial check in

---
+++ AD419.DataHelper.Web/Controllers/ProcessController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AD419.DataHelper.Web.Controllers
+{
+    public class ProcessController : SuperController
+    {
+        // GET: Process
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AD419.DataHelper.Web/Models/ProcessCategory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AD419.DataHelper.Web.Models
+{
+    [Table("ProcessCategory")]
+    public class ProcessCategory
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(200)]
+        public string Name { get; set; }
+
+        [DisplayName("Is Completed?")]
+        public bool IsCompleted { get; set; }
+
+        public string Notes { get; set; }
+
+        [MaxLength(200)]
+        public string StoredProcedureName { get; set; }
+    }
+}
