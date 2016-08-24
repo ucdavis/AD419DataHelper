983d281c61c632bf5158b344d8f5291715e1f228
Jason Sylvestre <jsylvestre@ucdavis.edu>
2016-08-24 14:30:14 -0700

    Call sproc.

--- AD419.DataHelper.Web/Controllers/StatusController.cs
+++ AD419.DataHelper.Web/Controllers/StatusController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -23,6 +24,7 @@ namespace AD419.DataHelper.Web.Controllers
                 return HttpNotFound();
             }
             category.IsCompleted = true;
+
             DbContext.SaveChanges();
 
             return RedirectToAction("Index");
@@ -37,7 +39,38 @@ namespace AD419.DataHelper.Web.Controllers
                 return HttpNotFound();
             }
 
-            return View(category);
+            try
+            {
+                ExecuteSproc(category.StoredProcedureName);
+            }
+            catch (Exception)
+            {
+                Message = "Error Running it";
+                return View(category);
+            }
+            
+
+            category.IsCompleted = true;
+
+            DbContext.SaveChanges();
+
+            Message = "Done";
+            return RedirectToAction("Index");
+
         }
+
+
+        private void ExecuteSproc(string sprocName)
+        {
+            var year = FiscalYearService.FiscalYear;
+            SqlParameter param1 = new SqlParameter("@FiscalYear", year);
+            SqlParameter param2 = new SqlParameter("@IsDebug", 0);
+
+
+
+            DbContext.Database.ExecuteSqlCommand(string.Format("{0} @FiscalYear @IsDebug", sprocName), param1, param2);
+
+        }
+
     }
 }
