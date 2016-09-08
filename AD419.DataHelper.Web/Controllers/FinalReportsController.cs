f4f337ca9199877aa96d8275bae25ead0a2d9826
Ken Taylor <kentaylor@ucdavis.edu>
2016-09-07 17:45:53 -0700

    Added the reports to this branch by mistake.  Will need to reopen again.

---
+++ AD419.DataHelper.Web/Controllers/FinalReportsController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using Microsoft.Reporting.WebForms;
+
+namespace AD419.DataHelper.Web.Controllers
+{
+    public class FinalReportsController : Controller
+    {
+        // GET: FinalReports
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NonAdminReport()
+        {
+            var reportViewer = new ReportViewer
+            {
+                SizeToReportContent = true,
+                Width = Unit.Percentage(100),
+                Height = Unit.Percentage(100),
+                ProcessingMode = ProcessingMode.Remote
+            };
+
+            reportViewer.ServerReport.ReportPath = "/AD419Reports/AD-419 Non-Admin Report";
+            reportViewer.ServerReport.ReportServerUrl =
+                new Uri("http://testreports.caes.ucdavis.edu/ReportServer/");
+
+            ViewBag.ReportViewer = reportViewer;
+            return View();
+        }
+
+        public ActionResult AdminReport()
+        {
+            var reportViewer = new ReportViewer
+            {
+                SizeToReportContent = true,
+                Width = Unit.Percentage(100),
+                Height = Unit.Percentage(100),
+                ProcessingMode = ProcessingMode.Remote
+            };
+
+            reportViewer.ServerReport.ReportPath = "/AD419Reports/AD-419 Admin Report - multi report";
+            reportViewer.ServerReport.ReportServerUrl =
+                new Uri("http://testreports.caes.ucdavis.edu/ReportServer/");
+
+            ViewBag.ReportViewer = reportViewer;
+            return View();
+        }
+
+        public ActionResult NonAdminReportWithProrateAmounts()
+        {
+            var reportViewer = new ReportViewer
+            {
+                SizeToReportContent = true,
+                Width = Unit.Percentage(100),
+                Height = Unit.Percentage(100),
+                ProcessingMode = ProcessingMode.Remote
+            };
+
+            reportViewer.ServerReport.ReportPath = "/AD419Reports/AD-419 Non-Admin Report with Prorate Amounts - multi report";
+            reportViewer.ServerReport.ReportServerUrl =
+                new Uri("http://testreports.caes.ucdavis.edu/ReportServer/");
+
+            ViewBag.ReportViewer = reportViewer;
+            return View();
+        }
+
+        public ActionResult UnassociatedTotalsReport()
+        {
+            var reportViewer = new ReportViewer
+            {
+                SizeToReportContent = true,
+                Width = Unit.Percentage(100),
+                Height = Unit.Percentage(100),
+                ProcessingMode = ProcessingMode.Remote
+            };
+
+            reportViewer.ServerReport.ReportPath = "/AD419Reports/AD-419 Unassociated Totals - multi report";
+            reportViewer.ServerReport.ReportServerUrl =
+                new Uri("http://testreports.caes.ucdavis.edu/ReportServer/");
+
+            ViewBag.ReportViewer = reportViewer;
+            return View();
+        }
+    }
+}
