6b3ba3a6373e4ac5de35e23460bbeea82afcee96
Ken Taylor <kentaylor@ucdavis.edu>
2016-11-01 11:45:49 -0700

    Added list of projects with unknown statuses.

---
+++ AD419.DataHelper.Web/Models/ProjectStatusViewModel.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Collections.Generic;
+
+namespace AD419.DataHelper.Web.Models
+{
+    public class ProjectStatusViewModel
+    {
+        public List<CurrentAd419Project> CurrentAd419Projects { get; set; }
+
+        public List<ProjectStatus> ProjectStatuses { get; set; }
+    }
+}
