9d940165888e00fd23f7c262de740783e828c6a0
Ken Taylor <kentaylor@ucdavis.edu>
2016-11-10 14:33:02 -0800

    Added new view and controller to compare ARC expenses Vs Adjusted AD-419 Total Research Expenses fixes issue #174

---
+++ AD419.DataHelper.Web/Controllers/AdjustedTotalsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using AD419.DataHelper.Web.Models;
+
+namespace AD419.DataHelper.Web.Controllers
+{
+    /// <summary>
+    /// Displays a list of adjustments that were made in order to make a side-by-side
+    /// comparison between the amount displayed in the AD-419 UI and the FFY Expense
+    /// by ARC totals.
+    /// </summary>
+    public class AdjustedTotalsController : SuperController
+    {
+        // GET: AdjustedTotals
+        public ActionResult Index()
+        {
+            var model = new ExpenseAdjustments
+            {
+                FieldStationExpenses = GetFieldStationExpenses(),
+                CeSpecialistExpenses = GetCeSpecialistExpenses(),
+                Expired204Expenses = GetExpired204Expenses(),
+                OutsideArcExpenses = GetOutsideArcExpenses(),
+                TotalProjectLessThanCutoffAmount204Expenses = GetTotalProjectLessThanCutoffAmount204Expenses(),
+                TotalFfyExpensesByArc = GetTotalFfyExpensesByArc(),
+                TotalAllResearchFundsAd419 = GetTotalAllResearchFundsAd419(),
+            };
+
+
+            return View(model);
+        }
+
+        private decimal GetFieldStationExpenses()
+        {
+            var query = DbContext.Database.SqlQuery<decimal>(
+                @"
+        SELECT ISNULL(SUM(Expenses),0) FieldStationExpenses
+	    FROM [dbo].[AllExpenses] 
+	    WHERE [DataSource] = '22F'");
+
+            var retval = query.FirstOrDefault();
+
+            return retval;
+        }
+
+        private decimal GetCeSpecialistExpenses()
+        {
+            var query = DbContext.Database.SqlQuery<decimal>(
+                @"
+        SELECT ISNULL(SUM(Expenses),0) CeSpecialistExpenses
+	    FROM [dbo].[AllExpenses] 
+	    WHERE [DataSource] = 'CES'");
+
+            var retval = query.FirstOrDefault();
+
+            return retval;
+        }
+
+        private decimal GetOutsideArcExpenses()
+        {
+            var query = DbContext.Database.SqlQuery<decimal>(
+                @"
+        SELECT ISNULL(SUM(Expenses),0) '204ExpensesOutsideArcFromAllExpenses'
+	    FROM AllExpenses
+        WHERE Chart+Account IN (
+            SELECT Chart+Account  
+            FROM AllAccountsFor204Projects
+            WHERE IsExpired = 0 AND ExcludedByARC = 1 AND ExcludedByAccount = 0)");
+
+            var retval = query.FirstOrDefault();
+
+            return retval;
+        }
+
+        private decimal GetExpired204Expenses()
+        {
+            var query = DbContext.Database.SqlQuery<decimal>(
+                @"
+        SELECT ISNULL(sum(Expenses),0) 'Expired204Expenses'
+        FROM AllAccountsFor204Projects
+        WHERE IsExpired = 1 AND ExcludedByARC = 0 AND ExcludedByAccount = 0");
+
+            var retval = query.FirstOrDefault();
+
+            return retval;
+        }
+
+        private decimal GetTotalProjectLessThanCutoffAmount204Expenses()
+        {
+            var query = DbContext.Database.SqlQuery<decimal>(
+                @"
+        SELECT ISNULL(sum(Expenses),0) Excluded204Expenses 
+        from AllAccountsFor204Projects 
+        where AccessionNumber IN (
+            select AccessionNumber 
+            from allProjectsNew
+            where  Is204 = 1 and IsExpired = 0 AND IsIgnored = 1)");
+
+            var retval = query.FirstOrDefault();
+
+            return retval;
+        }
+
+        private decimal GetTotalFfyExpensesByArc()
+        {
+            var query = DbContext.Database.SqlQuery<decimal>(String.Format(
+                @"
+        SELECT ISNULL(sum(Total),0) TotalFfyExpensesByArc 
+        from FFY_ExpensesByARC
+        WHERE Chart+Account NOT IN (
+            SELECT Chart+Account
+            FROM ARCCodeAccountExclusions
+            WHERE Year = {0})", FiscalYearService.FiscalYear));
+
+            var retval = query.FirstOrDefault();
+
+            return retval;
+        }
+
+        private decimal GetTotalAllResearchFundsAd419()
+        {
+            var query = DbContext.Database.SqlQuery<decimal>(
+               @"
+        SELECT ISNULL(sum(Expenses),0) TotalAd419Expenses 
+        from Expenses");
+
+            var retval = query.FirstOrDefault();
+
+            return retval;
+        }
+    }
+}
---
+++ AD419.DataHelper.Web/Models/ExpenseAdjustments.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AD419.DataHelper.Web.Models
+{
+    /// <summary>
+    /// Contains the various adjusts needed to be able to match the FFY totals and the AD-419 UI totals.
+    /// </summary>
+    public class ExpenseAdjustments
+    {
+    
+        [Display(Name = "Field Station Expenses")]
+        [DisplayFormat(DataFormatString = "{0:0,0.00}")]
+        public decimal FieldStationExpenses { get; set; }
+
+        [Display(Name = "CE Specialist Expenses")]
+        [DisplayFormat(DataFormatString = "{0:0,0.00}")]
+        public decimal CeSpecialistExpenses { get; set; }
+
+        [Display(Name = "Outside ARC 204 Expenses")]
+        [DisplayFormat(DataFormatString = "{0:0,0.00}")]
+        public decimal OutsideArcExpenses { get; set; }
+
+        [Display(Name = "Expired 204 Expenses")]
+        [DisplayFormat(DataFormatString = "{0:0,0.00}")]
+        public decimal Expired204Expenses { get; set; }
+
+        [Display(Name = "Total Project Expenses Less Than Cutoff Amount Expenses")]
+        [DisplayFormat(DataFormatString = "{0:0,0.00}")]
+        public decimal TotalProjectLessThanCutoffAmount204Expenses { get; set; }
+
+        [Display(Name = "Total All Research Funds (AD-419)")]
+        [DisplayFormat(DataFormatString = "{0:0,0.00}")]
+        public decimal TotalAllResearchFundsAd419 { get; set; }
+
+        [Display(Name = "Total FFY Expenses by ARC")]
+        [DisplayFormat(DataFormatString = "{0:0,0.00}")]
+        public decimal TotalFfyExpensesByArc { get; set; }
+
+        [Display(Name = "Total Adjusted AD-419 Expenses")]
+        [DisplayFormat(DataFormatString = "{0:0,0.00}")]
+        public decimal TotalAdjustedAd419Expenses
+        {
+            get
+            {
+                var retval = TotalAllResearchFundsAd419 -
+                             FieldStationExpenses -
+                             CeSpecialistExpenses -
+                             OutsideArcExpenses +
+                             Expired204Expenses +
+                             TotalProjectLessThanCutoffAmount204Expenses;
+
+                return retval;
+            }
+        }
+
+        [Display(Name = "Difference between total FFY Expenses and total Adjusted AD-419 Expenses")]
+        [DisplayFormat(DataFormatString = "{0:0,0.00}")]
+        public decimal DifferenceBetweenExpenseTotals
+        {
+            get { return TotalFfyExpensesByArc - TotalAdjustedAd419Expenses; }
+        }
+    }
+}
