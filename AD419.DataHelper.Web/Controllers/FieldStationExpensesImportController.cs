ef733858b075c314dc144a89c5da560c9babd3d2
Ken Taylor <kentaylor@ucdavis.edu>
2016-11-14 16:39:27 -0800

    Initial check in.

--- AD419.DataHelper.Web/Controllers/FieldStationExpensesImportController.cs
+++ AD419.DataHelper.Web/Controllers/FieldStationExpensesImportController.cs
@@ -6,12 +6,20 @@ using System.Web;
 using System.Web.Mvc;
 using AD419.DataHelper.Web.Helpers;
 using AD419.DataHelper.Web.Models;
+using AD419.DataHelper.Web.Services;
 using Excel;
 
 namespace AD419.DataHelper.Web.Controllers
 {
     public class FieldStationExpensesImportController : SuperController
     {
+        private readonly FieldStationExpenseService _fieldStationExpenseService;
+
+        public FieldStationExpensesImportController()
+        {
+            _fieldStationExpenseService = new FieldStationExpenseService(DbContext, FiscalYearService.FiscalEndDate);
+        }
+
         // GET: FieldStationExpensesImport
         public ActionResult Index()
         {
@@ -127,26 +135,43 @@ namespace AD419.DataHelper.Web.Controllers
             var myFile = Request.Files[0];
             if (myFile == null) return RedirectToAction("Index");
 
-            var fieldStationExpenseEntries = new List<FieldStationExpenseListImport>();
-
             var fileName = myFile.FileName;
+
+            // setup reader:
             var excelReader = ExcelReaderFactory.CreateOpenXmlReader(myFile.InputStream);
             excelReader.IsFirstRowAsColumnNames = true;
+
+            // read data:
             var result = excelReader.AsDataSet();
+            excelReader.Close();
+
+            // transform:
+            var fieldStationExpenses = _fieldStationExpenseService.GetFieldStationsExpensesFromRows(result.Tables[0].Rows).ToList();
+
+            // validate:
+            var errors = new List<ModelStateDictionary>();
+            foreach (var fieldStationExpense in fieldStationExpenses)
+            {
+                // clear and check
+                ModelState.Clear();
+                TryValidateModel(fieldStationExpense);
+
+                if (fieldStationExpense.IsCurrentAd419Project == null ||
+                    (bool) !fieldStationExpense.IsCurrentAd419Project)
+                {
+                    ModelState.AddModelError("IsCurrentAd419Project", "This expense is not assigned to a current project!  Please select one that is not expired and is currently active.");
+                }
+
+                // copy out errors
+                var state = new ModelStateDictionary();
+                state.Merge(ModelState);
+                errors.Add(state);
+            }
+            ViewBag.Errors = errors;
 
             TempData.Add("Message", "Now viewing \"" + fileName + "\".");
-            excelReader.Close();
-            var range = from DataRow row in result.Tables[0].Rows where !string.IsNullOrWhiteSpace(row["FieldStationCharge"].ToString()) select new FieldStationExpenseListImport(row);
-            fieldStationExpenseEntries.AddRange(range);
 
-            // This works.  Would now like the user to have a chance to review upload first.
-            //if (ModelState.IsValid)
-            //{
-            //    db.FieldStationExpenseListImport.AddRange(fieldStationExpenseEntries);
-            //    db.SaveChanges();
-            //}
-
-            return View("Display", fieldStationExpenseEntries);
+            return View("Display", fieldStationExpenses);
         }
 
         [HttpPost]
---
+++ AD419.DataHelper.Web/Services/FieldStationExpenseService.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using AD419.DataHelper.Web.Helpers;
+using AD419.DataHelper.Web.Models;
+
+namespace AD419.DataHelper.Web.Services
+{
+    public class FieldStationExpenseService
+    {
+        private readonly AD419DataContext _dataContext;
+        private static DateTime _fiscalEndDate;
+
+        public FieldStationExpenseService(AD419DataContext dataContext, DateTime fiscalEndDate)
+        {
+            _dataContext = dataContext;
+            _fiscalEndDate = fiscalEndDate;
+        }
+
+        public IEnumerable<FieldStationExpenseListImport> GetFieldStationsExpensesFromRows(DataRowCollection rows)
+        {
+            var allProjects = _dataContext.AllProjectsNew.Where(p => p.IsUcDavis).ToList();
+
+            var fieldStationExpenses = rows.ToEnumerable().Select(GetFieldStationExpenseFromRow).ToList();
+
+            var matchingProjects =
+                from fieldStationExpense in fieldStationExpenses
+                join project in allProjects on fieldStationExpense.ProjectAccessionNum equals project.AccessionNumber
+                into j
+                from match in j.DefaultIfEmpty()
+                select new
+                {
+                    FieldStationExpense = fieldStationExpense,
+                    Project = match
+                };
+
+            foreach (var match in matchingProjects)
+            {
+                ConfigureFieldStationExpense(match.FieldStationExpense, match.Project);
+            }
+
+            //var correspondingProjects =
+            //    fieldStationExpenses.Where(f => _dataContext.AllProjectsNew.Where(p => p.IsUcDavis).
+            //        All(p => f.ProjectAccessionNum.Equals(p.AccessionNumber)));
+           
+         
+            return fieldStationExpenses;
+        }
+
+        private void ConfigureFieldStationExpense(FieldStationExpenseListImport fieldStationExpense, AllProjectsNew project)
+        {
+            if (project != null)
+            {
+                // Use the project number provided; otherwise update if blank:
+                if (string.IsNullOrWhiteSpace(fieldStationExpense.ProjectNumber))
+                    fieldStationExpense.ProjectNumber = project.ProjectNumber;
+
+                // Determine if the expense belongs to a current project:
+                fieldStationExpense.IsCurrentAd419Project = IsCurrentAd419Project(project.ProjectStartDate, project.IsExpired);
+            }
+        }
+
+        private FieldStationExpenseListImport GetFieldStationExpenseFromRow(DataRow row)
+        {
+            var fieldStationExpense = new FieldStationExpenseListImport()
+            {
+                ProjectAccessionNum= row["ProjectAccessionNum"].ToString(),
+                ProjectNumber      = row["Project"].ToString(),
+                FieldStationCharge = string.IsNullOrWhiteSpace(row["FieldStationCharge"].ToString()) ? 0 : Convert.ToDecimal(row["FieldStationCharge"].ToString())
+            };
+
+            // fix accession number
+            if (string.IsNullOrWhiteSpace(fieldStationExpense.ProjectAccessionNum))
+            {
+                fieldStationExpense.ProjectAccessionNum = "0000000";
+            }
+            fieldStationExpense.ProjectAccessionNum = fieldStationExpense.ProjectAccessionNum.PadLeft(7, '0');
+
+            return fieldStationExpense;
+        }
+
+        /// <summary>
+        /// Project is current if not expired and start date is not in the future.
+        /// </summary>
+        /// <param name="startDate"></param>
+        /// <param name="isExpired"></param>
+        /// <returns></returns>
+        private static bool IsCurrentAd419Project(DateTime? startDate, bool isExpired)
+        {
+            if (startDate == null)
+                return false;
+
+            return startDate < _fiscalEndDate && !isExpired;
+        }
+    }
+}
