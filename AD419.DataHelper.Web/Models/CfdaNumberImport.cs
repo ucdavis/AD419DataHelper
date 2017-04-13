134fdc4f9d84cd0975ba932a774b871d4dfa32bc
Ken Taylor <kentaylor@ucdavis.edu>
2017-04-13 11:20:43 -0700

    Changes to display new CFDA num import fields

--- AD419.DataHelper.Web/Models/CfdaNumberImport.cs
+++ AD419.DataHelper.Web/Models/CfdaNumberImport.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data;
@@ -7,11 +8,18 @@ namespace AD419.DataHelper.Web.Models
     [Table("CFDANumImport")]
     public partial class CfdaNumberImport
     {
+        const string USDA   = "Department of Agriculture";
+        const string NIFA   = "National Institute of Food and Agriculture";
+        const string DHHS   = "Department of Health and Human Services";
+        const string NIH    = "National Institutes of Health";
+        
         public CfdaNumberImport() { }
         public CfdaNumberImport(DataRow row)
         {
-            Number = row["CFDANum"].ToString();
-            ProgramTitle = row["ProgramTitle"].ToString();
+            Number = row["#"].ToString();
+            ProgramTitle = row["Program Title"].ToString();
+            AgencyOffice = row["Agency/Office"].ToString();
+            Code = GetCode(AgencyOffice);
         }
 
         public int Id { get; set; }
@@ -26,5 +34,35 @@ namespace AD419.DataHelper.Web.Models
         [Display(Name = "Program Title")]
         [StringLength(300)]
         public string ProgramTitle { get; set; }
+
+        [Required]
+        [Display(Name = "Agency/Office")]
+        [StringLength(2048)]
+        public string AgencyOffice { get; set; }
+
+        [Display(Name = "Code")]
+        [StringLength(10)]
+        public string Code { get; set; }
+
+        /// <summary>
+        /// Sets the Code based of the Agency/Office for NIFA and NIH.
+        /// </summary>
+        /// <param name="agencyOffice"></param>
+        private string GetCode(string agencyOffice)
+        {
+            if (string.IsNullOrEmpty(agencyOffice))
+                return null;
+
+            if (agencyOffice.StartsWith(USDA) && agencyOffice.Contains(NIFA))
+            {
+                return "NIFA";
+            }
+            else if (agencyOffice.StartsWith(DHHS) && agencyOffice.Contains(NIH))
+            {
+                return "NIH";
+            }
+
+            return null;
+        }
     }
 }
