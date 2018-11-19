28eb4af024342109eff89c8560603c1889ec838d
Ken Taylor <kentaylor@ucdavis.edu>
2018-11-19 11:31:54 -0800

    Added method to fetch accounts with missing SFN from database.

---
+++ AD419.DataHelper.Web/Controllers/AccountsWithMissingSfnController.cs
@@ -0,0 +1,48 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using AD419.DataHelper.Web.Models;
+
+namespace AD419.DataHelper.Web.Controllers
+{
+    public class AccountsWithMissingSfnController : SuperController
+    {
+        // GET: AccountsWithMissingSfn
+        public ActionResult Index()
+        {
+            return View(DbContext.GetAccountsWithMissingSfn().ToList());
+        }
+
+        // GET: AccountsWithMissingSfn/Edit/5
+        public ActionResult Edit(string Chart, string Account)
+        {
+            if (string.IsNullOrWhiteSpace(Chart) || string.IsNullOrWhiteSpace(Account))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var accountWithMissingSfn = DbContext.NewAccountSfns.Find(Chart, Account);
+            if (accountWithMissingSfn == null)
+            {
+                return HttpNotFound();
+            }
+            return View(accountWithMissingSfn);
+        }
+
+        // POST: AccountsWithMissingSfn/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Sfn")] NewAccountSfn accountWithMissingSfn)
+        {
+            if (ModelState.IsValid)
+            {
+                DbContext.Entry(accountWithMissingSfn).State = EntityState.Modified;
+                DbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(accountWithMissingSfn);
+        }
+    }
+}
---
+++ AD419.DataHelper.Web/Models/NewAccountSfn.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace AD419.DataHelper.Web.Models
+{
+    public partial class NewAccountSfn
+    {
+        [Column(Order = 0), Key]
+        public string Chart { get; set; }
+
+        [Column(Order = 1), Key]
+        public string Account { get; set; }
+
+        [Display(Name = "Org")]
+        public string Org { get; set; }
+
+        [Display(Name = "Is CE?")]
+        public bool IsCE { get; set; }
+
+        [Required]
+        [Display(Name = "SFN")]
+        public bool SFN { get; set; }
+
+        [Display(Name = "CFDA Num")]
+        public string CFDANum { get; set; }
+
+        [Display(Name = "OP Fund Group Code")]
+        public string OpFundGroupCode { get; set; }
+
+        [Display(Name = "OP Fund Num")]
+        public string OpFundNum { get; set; }
+
+        [Display(Name = "Federal Agency Code")]
+        public string FederalAgencyCode { get; set; }
+
+        [Display(Name = "NIH Doc Num")]
+        public string NIHDocNum { get; set; }
+
+        [Display(Name = "Sponsor Category Code")]
+        public string SponsorCategoryCode { get; set; }
+
+        [Display(Name = "Sponsor Code")]
+        public string SponsorCode { get; set; }
+
+        [Display(Name = "Accounts Award Num")]
+        public string Accounts_AwardNum { get; set; }
+
+        [Display(Name = "Op Fund Award Num")]
+        public string OpFund_AwardNum { get; set; }
+
+        [Display(Name = "Expiration Date")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime ExpirationDate { get; set; }
+
+        [Display(Name = "Award End Date")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime AwardEndDate { get; set; }
+
+        [Display(Name = "Is Account In Financial Data?")]
+        public bool IsAccountInFinancialData { get; set; }
+
+        [Display(Name = "Sub-Fund Group Num")]
+        public string SubFundGroupNum { get; set; }
+
+        [Display(Name = "Sub-Fund Group Type Code")]
+        public string SubFundGroupTypeCode { get; set; }
+
+        [Display(Name = "Is NIH?")]
+        public bool IsNIH { get; set; }
+
+        [Display(Name = "Is Federal Fund?")]
+        public bool IsFederalFund { get; set; }
+
+        [Display(Name = "Is NIFA?")]
+        public bool IsNIFA { get; set; }
+    }
+}
