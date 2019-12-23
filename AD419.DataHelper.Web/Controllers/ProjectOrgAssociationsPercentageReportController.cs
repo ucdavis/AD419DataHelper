10c03bd23340789f188619ca39aa78fda01b9fcf
Ken Taylor <kentaylor@ucdavis.edu>
2019-12-23 14:32:23 -0800

    Added the controller and page for Project/Org Associations Percentage Report

---
+++ AD419.DataHelper.Web/Controllers/ProjectOrgAssociationsPercentageReportController.cs
@@ -0,0 +1,22 @@
+﻿using AD419.DataHelper.Web.Models;
+using System.Web.Mvc;
+
+namespace AD419.DataHelper.Web.Controllers
+{
+    public class ProjectOrgAssociationsPercentageReportController : SuperController
+    {
+        // GET: CurrentProjectsReport
+        public ActionResult Index()
+        {
+            var reportName = @"Project-Org Associations Percentage Report";
+            var model = new ReportViewerModel(reportName);
+
+            var endingYear = FiscalYearService.FiscalYear;
+            var startingYear = endingYear - 1;
+            var reportPeriodString = startingYear.ToString() + "-" + endingYear.ToString();
+            ViewBag.ReportingPeriod = reportPeriodString;
+
+            return View(model);
+        }
+    }
+}
