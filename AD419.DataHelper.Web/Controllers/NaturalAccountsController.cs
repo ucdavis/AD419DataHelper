5b3b321bc6da1209feda7d53e15b07b0ff926daa
Robert Martinsen <rmartinsen@ucdavis.edu>
2025-08-07 14:41:13 -0700

    Add Activity and Natural Account reports

---
+++ AD419.DataHelper.Web/Controllers/NaturalAccountsController.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using System.Net;
+using AD419.DataHelper.Web.Models;
+
+namespace AD419.DataHelper.Web.Controllers
+{
+    public class NaturalAccountsController : SuperController
+    {
+        // GET: NaturalAccounts
+        public ActionResult Index()
+        {
+            var model = new NaturalAccountsViewModel
+            {
+                NaturalAccounts = DbContext.NaturalAccount
+                    .OrderBy(a => a.IncludeInAD419.HasValue ? (a.IncludeInAD419.Value ? 0 : 1) : 2)  // Yes first, No second, NULL last
+                    .ThenBy(a => a.Account_Parent_1)
+                    .ThenBy(a => a.Account_Child_Posting_Level)
+                    .ToList(),
+                CodeTypeName = "Natural Accounts"
+            };
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var account = DbContext.NaturalAccount.Find(id);
+
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(account);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Account_Child_Posting_Level,IncludeInAD419")] NaturalAccount model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var existing = DbContext.NaturalAccount.Find(model.Account_Child_Posting_Level);
+            if (existing == null) return HttpNotFound();
+
+            existing.IncludeInAD419 = model.IncludeInAD419;
+
+            DbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ AD419.DataHelper.Web/Models/NaturalAccount.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AD419.DataHelper.Web.Models
+{
+    [Table("Natural_Acct")]
+    public class NaturalAccount
+    {
+        public string Account_Parent_5 { get; set; }
+        public string Account_Parent_5_Description { get; set; }
+
+        public string Account_Parent_4 { get; set; }
+        public string Account_Parent_4_Description { get; set; }
+
+        public string Account_Parent_3 { get; set; }
+        public string Account_Parent_3_Description { get; set; }
+
+        public string Account_Parent_2 { get; set; }
+        public string Account_Parent_2_Description { get; set; }
+
+        public string Account_Parent_1 { get; set; }
+        public string Account_Parent_1_Description { get; set; }
+
+        [Key]
+        public string Account_Child_Posting_Level { get; set; }
+        public string Account_Child_Posting_Description { get; set; }
+
+        [Column("Include_in_AD-419?")]
+        public bool? IncludeInAD419 { get; set; }
+
+        [Column("Present_in_Financial_Data?")]
+        public bool? PresentInFinancialData { get; set; }
+    }
+
+    public class NaturalAccountsViewModel
+    {
+        public List<NaturalAccount> NaturalAccounts { get; set; }
+        public string CodeTypeName { get; set; }
+    }
+}
